90591fed202f3d39dace27c500bb7b5a20c8843b
danielknox <dk242@kent.ac.uk>
2018-06-19 17:00:49 +0100

    Added gps functionality - depends on neogps

--- gps.cpp
+++ gps.cpp
@@ -1,43 +1,75 @@
 #include "gps.h"
+#include <NMEAGPS.h>
+#include <GPSport.h>
+#include <Streamers.h>
+#include "wiring_private.h" // pinPeripheral() function 
 
+// Define additional hardware serial port on M0
+Uart Serial2 (&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
+
+// Check neogps configuration
+
+#ifndef NMEAGPS_INTERRUPT_PROCESSING
+  #error You must define NMEAGPS_INTERRUPT_PROCESSING in NMEAGPS_cfg.h!
+#endif
+
+#ifndef GPS_FIX_ALTITUDE
+  #error You must define GPS_FIX_ALTITUDE in GPSfix_cfg.h!
+#endif
+
+#ifndef GPS_FIX_HDOP
+  #error You must define GPS_FIX_HDOP in GPSfix_cfg.h!
+#endif
+
+static NMEAGPS   gps;
+static gps_fix   fix;
+
+// Interupt handler required to feed characters to GPS
+void SERCOM1_Handler()
+{
+  Serial2.IrqHandler();
+  gps.handle( Serial2.read() );
+}
+
+/**************************************************************************/
+/*!
+    @brief  Starts the GPS
+*/
+/**************************************************************************/
+void initGPS(){
+  gpsPort.begin( 9600 );
+  pinPeripheral(10, PIO_SERCOM);
+  pinPeripheral(11, PIO_SERCOM);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Checks to see if GPS has a valid lock (location, alt and hdop)
+    @return True if gps fix is valid, false if it is not.
+*/
+/**************************************************************************/
 bool hasGPSLock() {
+  while (gps.available()) {
+    fix = gps.read();
+  }
+  if(fix.valid.location && fix.valid.altitude && fix.valid.hdop){  
+    return true;
+  }
   return false;
 }
 
-bool populateGPSPayload(char* payload) {
+/**************************************************************************/
+/*!
+    @brief  If GPS has a valid lock it will populate the passed structure with location data.
+    @param  Reference of a location struct to be populated 
+    @return True if gps data was available, otherwise false.
+*/
+/**************************************************************************/
+bool retieveGPS(loc* location) {
    if(!hasGPSLock()) return false;
-
-  //TODO : re-write following to match
-//  if (locInfo) {
-//    if(fix.valid.location){  
-//      frameFormat.gpsLoc = 1;
-//      LatitudeBinary = (fix.latitudeL() + 900000000 )/ 200;
-//      LongitudeBinary = (fix.latitudeL() + 1800000000 )/ 400;
-//      
-//      txBuffer[4] = (LatitudeBinary >> 16) & 0xFF;
-//      txBuffer[5] = (LatitudeBinary >> 8) & 0xFF;
-//      txBuffer[6] = LatitudeBinary & 0xFF;
-//
-//      txBuffer[7] = (LongitudeBinary >> 16) & 0xFF;
-//      txBuffer[8] = (LongitudeBinary >> 8) & 0xFF;
-//      txBuffer[9] = LongitudeBinary & 0xFF;
-//    } else {
-//      frameFormat.gpsLoc = 0;
-//    }
-//    if(fix.valid.altitude){
-//      frameFormat.gpsAlt = 1;
-//      altitudeGps = fix.alt.whole;    
-//      txBuffer[10] = (altitudeGps >> 8) & 0xFF;
-//      txBuffer[11] = altitudeGps & 0xFF;
-//    } else {
-//      frameFormat.gpsAlt = 0;
-//    }
-//    if(fix.valid.hdop){
-//      frameFormat.gpsHdop = 1;
-//      txBuffer[12] = (fix.hdop / 1000);
-//    } else {
-//      frameFormat.gpsHdop = 0;
-//    }
-//  }
-  return true;
+   location->latitude = fix.latitudeL();
+   location->longitude = fix.longitudeL();
+   location->altitude = fix.altitude_cm();    
+   location->hdop = fix.hdop;
+   return true;
 }
