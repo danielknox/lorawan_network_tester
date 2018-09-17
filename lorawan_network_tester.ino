25a7191d7a1c0918ef9f3cc3696b23c7680487a0
Daniel Knox <danielknox@users.noreply.github.com>
2018-09-17 14:01:42 +0100

    Update lorawan_network_tester.ino
    Improved the TTN decoder

--- lorawan_network_tester.ino
+++ lorawan_network_tester.ino
@@ -2,30 +2,27 @@
  *  Author: Keith Greenhow and Daniel Knox
  *  
  *  Decoder function for TTN:
- function Decoder(bytes, port) {
-  // Decode an uplink message from a buffer
-  // (array) of bytes to an object of fields.
-  var decoded = {};
+  function Decoder(bytes, port) {
+    // Decode an uplink message from a buffer
+    // (array) of bytes to an object of fields.
+    var decoded = {};
 
-  if (port === 1){
-    decoded = { 
-      "packetformat": {
-      "trigger_button":  bytes[0] &&  1 ,
-      "gps":  (bytes[0] >>> 1) && 1
+    if (port === 1){
+      decoded = { 
+        trigger_button:  !!(bytes[0] &  1) ,
+        gps:  !!(bytes[0] & 2)
       }
+      if(decoded.gps){
+        decoded.latitude = ((bytes[1]<<16)) + ((bytes[2]<<8)) + bytes[3];
+        decoded.latitude = ((decoded.latitude * 200) - 900000000) / 10000000;
+        decoded.longitude = ((bytes[4]<<16)) + ((bytes[5]<<8)) + bytes[6];
+        decoded.longitude =((decoded.longitude * 400 ) - 1800000000) / 10000000;
+        decoded.altitude = ((bytes[7]<<8)) + bytes[8];
+        decoded.hdop = bytes[9]/10; 
+      }
+    return decoded;
     }
-  if(bytes[0] >>> 1 && 1){
-    decoded.latitude = ((bytes[1]<<16)>>>0) + ((bytes[2]<<8)>>>0) + bytes[3];
-    decoded.latitude = ((decoded.latitude * 200) - 900000000) / 10000000;
-    decoded.longitude = ((bytes[4]<<16)>>>0) + ((bytes[5]<<8)>>>0) + bytes[6];
-    decoded.longitude =((decoded.longitude * 400 ) - 1800000000) / 10000000;
-    decoded.altitude = ((bytes[7]<<8)>>>0) + bytes[8];
-    decoded.hdop = bytes[9]/10; 
   }
-
-  return decoded;
-}
-}
  */
 
 #include "screen.h"
